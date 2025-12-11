96c7c8691e0684dd9480825138f293b0684feb96
Naman Mehrotra <namanlinkedin183@gmail.com>
2025-12-11 19:03:30 +0530

    Remove duplicates problem

--- Array_Problems/Remove_duplicates.java
+++ Array_Problems/Remove_duplicates.java
@@ -1,6 +1,6 @@
 // Brute force
 
-
+/*
 import java.util.Scanner;
 import java.util.Arrays;
 import java.util.HashSet;
@@ -37,6 +37,46 @@ public class Remove_duplicates {
 
         int result = removeduplicates(nums, n);
         System.out.println("Number of unique elements in the array: " + Arrays.toString(nums) + " are: " + result);
+        input.close();
+    }
+}
+    */
+
+// OPtimal approach
+
+import java.util.*;
+
+public class Remove_duplicates {
+
+       
+        public static int removeduplicates(int[] nums, int n){
+            
+            if(nums.length == 0) return 0;
+
+            int i = 0;
+            for(int j = 1; j < nums.length; j++){
+                if(nums[j] != nums[i]){
+                    i ++;
+
+                    nums[i] = nums[j];
+                }
+            }
+            return i+1;
+        }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the value for n: ");
+        int n = input.nextInt();
+
+        int[] nums = new int[n];
+        System.out.println("Enter the elemnts of the array: ");
+        for(int i = 0; i < nums.length; i++){
+            nums[i] = input.nextInt();
+        }
+
+        int result = removeduplicates(nums, n);
+
+        System.out.println("Nummber of uniques elements in the array: " + Arrays.toString(nums) + result);
         input.close();
     }
 }
