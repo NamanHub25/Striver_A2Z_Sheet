67d794c1b04c9e8af718688e0d8ef9b3877619b6
Naman Mehrotra <namanlinkedin183@gmail.com>
2025-12-11 07:51:02 +0530

    Check sorted array LC problem

--- Check_sorted.java
+++ Check_sorted.java
@@ -1,16 +1,19 @@
+
 import java.util.*;
 
 public class Check_sorted {
 
     public static boolean checksortedarray(int[] nums, int n){
-        boolean flag = false;
 
         for(int i = 1; i < nums.length; i++){
             if(nums[i] > nums[i-1]){
-                flag = true;
+
+            }
+            else{
+                return false;
             }
         }
-        return flag;
+        return true;
     }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -22,8 +25,10 @@ public class Check_sorted {
         for(int i = 0; i < nums.length; i++){
             nums[i] = input.nextInt();
         }
+
         boolean result = checksortedarray(nums, n);
-        System.out.println("Result for the array:  " + Arrays.toString(nums) + " is: " + result);
+
+        System.out.println("Result for the array: " + Arrays.toString(nums) + " is: " + result);
         input.close();
     }
 }
